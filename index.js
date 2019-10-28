75af65ffc90ab2283053d967c5f69f18248d56e2
DiegoArvz <darvz.gtz@gmail.com>
2019-10-28 09:37:12 -0600

    Caesar Cipher Encryption Function Implemented

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const caesarCipherEncryptor = (string, key) => {
+	const map = 'abcdefghijklmnopqrstuvwxyz'.split("");
+	let result = '';
+	for (let char of string) {
+		result += map[(map.indexOf(char) + key) % (map.length)]
+	}
+	return result;
+}
